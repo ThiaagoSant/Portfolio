0c393543fa3d77a44995aaee15938102f66f085c
Thiago Santiago <thiagosantiagoss20@gmail.com>
2020-11-12 17:43:57 -0300

    Adding the responsive navigation bar and media queries

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,21 @@
 $(document).ready(function(){
+	
 	$(window).on("scroll", function stickyNav(){
 		const windowTop = $(this).scrollTop(),
 					headerEl = $("header"),
 					stickyClass = "sticky";
 		
 		(windowTop > 20) ? headerEl.addClass(stickyClass) : headerEl.removeClass(stickyClass);
+	});
+	
+	// responsive navbar
+	$(".menu-btn i").on("click", function menuActive(){
+		const menu = $(".navbar ul"),
+					classActive = "active",
+					classClose = "fa-times";
+		
+		menu.toggleClass(classActive);
+		$(this).toggleClass(classClose);
 	});
 
 });
